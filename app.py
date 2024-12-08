6629ac49cc09cb32a0d15736beedcf61542d2b0d
fleabagreact <luizadomato16@gmail.com>
2024-12-08 18:46:46 -0300

    feat: adicionar página de preferência

--- app.py
+++ app.py
@@ -17,8 +17,19 @@ def login():
 def register():
     if request.method == 'POST':
         # registrar o usuário
-        return redirect(url_for('main'))
+        return redirect(url_for('preferencias'))
     return render_template('register.html')
+
+@app.route('/preferencias', methods=['POST', 'GET'])
+def preferencias():
+    if request.method == 'POST':
+        # obter preferências selecionadas
+        generos_selecionados = request.form.getlist('generos')
+        print(f"Gêneros selecionados: {generos_selecionados}")  # Apenas para debug
+        # pode salvar os dados no banco ou realizar outro processamento
+        # redirecionar para o main após salvar
+        return redirect(url_for('main'))
+    return render_template('preferencias.html')
 
 @app.route('/main')
 def main():
